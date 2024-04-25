ccfc7321eeb5f311f16153299d20c05d01608b94
Alvaro Soldevilla <alvarosoldevilla2005@gmail.com>
2024-04-25 19:27:32 +0200

    Changes by diego

--- ProyectoV1/src/main/java/Modelo/Misc/Estados.java
+++ ProyectoV1/src/main/java/Modelo/Misc/Estados.java
@@ -5,9 +5,11 @@ import lombok.Getter;
 @Getter
 public enum Estados {
 
-    VENENO("Venenno",true,true,2),
+    VENENO("Veneno",true,true,2),
     QUEMADURA("Quemadura",true,true,2),
-    CEGADO("Cegado",false,true,2);
+    CEGADO("Cegado",false,true,2),
+    MALDITO("Maldito", true, true, 2),
+    CONGELADO("Congelado", false, true, 1);
 
     Estados(String nombre, boolean puedeMultiples, boolean deterioro,int efecto) {
         this.nombre = nombre;
